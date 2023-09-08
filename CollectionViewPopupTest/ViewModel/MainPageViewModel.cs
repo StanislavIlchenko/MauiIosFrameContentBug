12c0e2bb203cd6dc81ee165adb0c6108d6f0ee9b
Stanislav Ilchenko <stanislav.ilchenko@ssa.group>
2023-09-08 17:12:49 +0300

    add popup with collection view

--- CollectionViewPopupTest/ViewModel/MainPageViewModel.cs
+++ CollectionViewPopupTest/ViewModel/MainPageViewModel.cs
@@ -10,11 +10,14 @@ namespace CollectionViewPopupTest.ViewModel
         public Command ShowCollectionView { get; set; }
         public Command SelectLanguageCommand { get; set; }
 
+        public Command SelectLanguageCommand_CollectionView { get; set; }
+
         public MainPageViewModel(IPopupDialogService popupDialogService) 
         {
             _popupDialogService = popupDialogService;
             ShowCollectionView = new Command(ShowCollectionViewPopup);
             SelectLanguageCommand = new Command(ShowSelectLanguagePopup);
+            SelectLanguageCommand_CollectionView = new Command(ShowSelectLanguagePopup_CollectionView);
         }
 
         private void ShowCollectionViewPopup()
@@ -26,10 +29,19 @@ namespace CollectionViewPopupTest.ViewModel
             _popupDialogService.ShowPopup(collectionView);
         }
 
-        private void ShowSelectLanguagePopup() 
+        private void ShowSelectLanguagePopup()
         {
             var languagesViewModel = new LanguageSelectPopupViewModel(_popupDialogService, Localization.AppResources.CurrentLanguage);
             var view = new LanguageSelectPopup();
+            view.BindingContext = languagesViewModel;
+
+            _popupDialogService.ShowPopup(view);
+        }
+
+        private void ShowSelectLanguagePopup_CollectionView()
+        {
+            var languagesViewModel = new LanguageSelectPopupViewModel(_popupDialogService, Localization.AppResources.CurrentLanguage);
+            var view = new LanguageSelectPopup_CollectionView();
             view.BindingContext = languagesViewModel;
 
             _popupDialogService.ShowPopup(view);
